ef562129159a457a7bd6ccb81a1b74153f123bca
HUYNH THAI DUONG <huynhthaiduong21042003@gmail.com>
2024-09-30 11:41:21 +0700

    deploy on azure

--- ClothesStoreMobileApplication/Program.cs
+++ ClothesStoreMobileApplication/Program.cs
@@ -96,11 +96,8 @@ var app = builder.Build();
 
 app.UseCors("AllowSpecificOrigin");
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // app.UseHttpsRedirection();
 

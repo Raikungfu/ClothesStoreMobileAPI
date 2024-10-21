8b73b7ae6fc29e3bd244b4bdaf63c948c505cb00
Raikungfu <raikungfu@gmail.com>
2024-10-21 17:03:07 +0700

    seller

---
+++ ClothesStoreMobileApplication/Controllers/SellerController.cs
@@ -0,0 +1,38 @@
+﻿using ClothesStoreMobileApplication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ClothesStoreMobileApplication.Controllers
+{
+
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+
+        private readonly ClothesStoreContext _context;
+        private readonly IConfiguration _configuration;
+
+        public SellerController(ClothesStoreContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSeller(int id)
+        {
+            var seller = await _context.Sellers
+                .Include(s => s.User)
+                .FirstOrDefaultAsync(s => s.SellerId == id);
+
+            if (seller == null)
+                return NotFound(new { Message = "Seller not found." });
+
+            return Ok(seller);
+        }
+    }
+}
--- ClothesStoreMobileApplication/Migrations/20241017005641_update-data.Designer.cs
+++ ClothesStoreMobileApplication/Migrations/20241017005641_update-data.Designer.cs
@@ -4,6 +4,7 @@ using ClothesStoreMobileApplication.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClothesStoreMobileApplication.Migrations
 {
     [DbContext(typeof(ClothesStoreContext))]
-    partial class ClothesStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20241017005641_update-data")]
+    partial class updatedata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

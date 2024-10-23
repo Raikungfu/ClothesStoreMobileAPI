58c843a2a05c31c4bbd35538b8577ab9ecd5d40e
doantrang9303 <133722717+doantrang9303@users.noreply.github.com>
2024-10-23 21:42:04 +0700

    update

--- ClothesStoreMobileApplication/ViewModels/Order/OrderCreateViewModel.cs
+++ ClothesStoreMobileApplication/ViewModels/Order/OrderCreateViewModel.cs
@@ -3,21 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ClothesStoreMobileApplication.ViewModels.Order
 {
-    public class OrderCreateViewModel
+     public class OrderCreateViewModel
     {
-        public int CustomerId { get; set; }
-
-        public string? ShipName { get; set; }
-
-        public string? ShipMail { get; set; }
-
-        public string? ShipPhone { get; set; }
-
-        public string? ShipAddress { get; set; }
-
         public string? DiscountCode { get; set; }
 
+        [Required]
         public string PaymentMethod { get; set; } = "direct";
-
     }
 }

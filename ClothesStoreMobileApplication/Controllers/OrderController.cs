b501cbab0b9d38d947a6bdf1d34eda9025e77dce
doantrang9303 <133722717+doantrang9303@users.noreply.github.com>
2024-10-23 21:35:21 +0700

    update create order

--- ClothesStoreMobileApplication/Controllers/OrderController.cs
+++ ClothesStoreMobileApplication/Controllers/OrderController.cs
@@ -217,8 +217,7 @@ namespace ClothesStoreMobileApplication.Controllers
 //     }
 // }
 
-  
-        [HttpPost]
+          [HttpPost]
         public IActionResult Post([FromBody] OrderCreateViewModel OrderVM)
         {
             try
@@ -240,122 +239,87 @@ namespace ClothesStoreMobileApplication.Controllers
                     return NotFound("Customer not found.");
                 }
 
-                //Check Cart của Customer có tồn tại không
-                //Nếu không thì trả về BadRequest
-                //Nếu có thì mới tiến hành tạo Order
-
                 var cart = _unitOfWork.Cart.GetFirstOrDefault(u => u.CustomerId == customer.CustomerId);
                 if (cart == null)
                 {
                     return BadRequest("Cart của Customer không tồn tại");
                 }
-                else
+
+                Order order = new Order
                 {
-                    //Tạo mới Order
-                    Order order = _mapper.Map<Order>(OrderVM);
-                    order.CustomerId = customer.CustomerId;
-                    if (string.IsNullOrEmpty(OrderVM.ShipName))
+                    CustomerId = customer.CustomerId,
+                    ShipName = customer.Name,
+                    ShipAddress = customer.Address,
+                    ShipPhone = customer.User?.Phone,
+                    ShipMail = customer.User?.Email,
+                    OrderDate = DateTime.Now,
+                    PaymentMethod = OrderVM.PaymentMethod
+                };
+
+                _unitOfWork.Order.Add(order);
+                _unitOfWork.Save();
+
+                // Xử lý các CartItem thành OrderItem
+                var cartItems = _unitOfWork.CartItem.GetAll(u => u.CartId == cart.CartId, includeProperty: "Product");
+                foreach (var item in cartItems)
+                {
+                    if (item.Product.Quantity < item.Quantity)
                     {
-                        if(customer.Name != null)
-                        {
-                            order.ShipName = customer.Name;
-                        }
-                        else
-                        {
-                            return BadRequest("Please update your Customer Information or Enter your Ship Name!");
-                        }
-                    }
-                    if (string.IsNullOrEmpty(OrderVM.ShipAddress))
-                    {
-                        if (customer.Address != null)
-                        {
-                            order.ShipAddress = customer.Address;
-                        }
-                        else
-                        {
-                            return BadRequest("Please update your Customer Information or Enter your Ship Address!");
-                        }
-                    }
-                    if (string.IsNullOrEmpty(OrderVM.ShipPhone))
-                    {
-                        order.ShipPhone = customer.User?.Phone;
+                        return BadRequest("Số lượng sản phẩm trong kho không đủ");
                     }
 
-                    if (string.IsNullOrEmpty(OrderVM.ShipMail))
+                    OrderItem orderItem = new OrderItem
                     {
-                        order.ShipMail = customer.User?.Email; 
-                    }
-
-                    order.OrderDate = DateTime.Now;
-                    order.PaymentMethod = OrderVM.PaymentMethod;
-                    _unitOfWork.Order.Add(order);
+                        OrderId = order.OrderId,
+                        ProductId = item.ProductId,
+                        Quantity = item.Quantity
+                    };
+                    _unitOfWork.OrderItem.Add(orderItem);
                     _unitOfWork.Save();
 
-                    //Add Order item từ CartItem
-                    var cartItems = _unitOfWork.CartItem.GetAll(u => u.CartId == cart.CartId, includeProperty: "Product");
-                    foreach (var item in cartItems)
-                    {
-                        //check số lượng sản phẩm trong kho
-                        if (item.Product.Quantity < item.Quantity)
-                        {
-                            return BadRequest("Số lượng sản phẩm trong kho không đủ");
-                        }
-                        else
-                        {
-                            OrderItem orderItem = new OrderItem
-                            {
-                                OrderId = order.OrderId,
-                                ProductId = item.ProductId,
-                                Quantity = item.Quantity
-                            };
-                            _unitOfWork.OrderItem.Add(orderItem);
-                            _unitOfWork.Save();
-                            //Trừ số lượng sản phẩm trong kho
-                            item.Product.Quantity -= item.Quantity;
-                            item.Product.QuantitySold += (uint)item.Quantity;
-                            _unitOfWork.Product.Update(item.Product);
-                            _unitOfWork.Save();
-                        }
-                        //Xoas CartItem
-                        _unitOfWork.CartItem.Remove(item);
-                        _unitOfWork.Save();
-                    }
-
-                    //Tính tổng tiền
-                    order.TotalAmount = 0;
-                    foreach (var item in _unitOfWork.OrderItem.GetAll(u => u.OrderId == order.OrderId, includeProperty: "Product"))
-                    {
-                        order.TotalAmount += item.Product.NewPrice * item.Quantity;
-                    }
-
-                    //Check DiscountCode
-                    if (!string.IsNullOrEmpty(OrderVM.DiscountCode))
-                    {
-                        var discount = _unitOfWork.Discount.GetFirstOrDefault(u => u.Code == OrderVM.DiscountCode);
-                        if (discount == null)
-                        {
-                            order.DiscountCode = "";
-                        }
-                        else
-                        {
-                            order.DiscountCode = OrderVM.DiscountCode;
-                            //update Discount
-                            discount.Quantity--;
-                            _unitOfWork.Discount.Update(discount);
-                            _unitOfWork.Save();
-                            //Tính lại TotalAmount
-                            order.TotalAmount = order.TotalAmount /100 *(100-(double)discount.DiscountPercentage);
-                        }
-                    }
-                    _unitOfWork.Order.Update(order);
+                    // Trừ số lượng sản phẩm trong kho
+                    item.Product.Quantity -= item.Quantity;
+                    item.Product.QuantitySold += (uint)item.Quantity;
+                    _unitOfWork.Product.Update(item.Product);
                     _unitOfWork.Save();
 
-                    //Xoas Cart
-                    _unitOfWork.Cart.Remove(cart);
+                    // Xóa CartItem
+                    _unitOfWork.CartItem.Remove(item);
                     _unitOfWork.Save();
-
-                    return Ok(order);
                 }
+
+                // Tính tổng tiền
+                order.TotalAmount = 0;
+                foreach (var item in _unitOfWork.OrderItem.GetAll(u => u.OrderId == order.OrderId, includeProperty: "Product"))
+                {
+                    order.TotalAmount += item.Product.NewPrice * item.Quantity;
+                }
+
+                // Xử lý mã giảm giá (nếu có)
+                if (!string.IsNullOrEmpty(OrderVM.DiscountCode))
+                {
+                    var discount = _unitOfWork.Discount.GetFirstOrDefault(u => u.Code == OrderVM.DiscountCode);
+                    if (discount != null)
+                    {
+                        order.DiscountCode = OrderVM.DiscountCode;
+                        discount.Quantity--;
+                        _unitOfWork.Discount.Update(discount);
+                        _unitOfWork.Save();
+
+                        // Tính lại TotalAmount với Discount
+                        order.TotalAmount = order.TotalAmount / 100 * (100 - (double)discount.DiscountPercentage);
+                    }
+                }
+
+                // Cập nhật Order với tổng số tiền cuối cùng
+                _unitOfWork.Order.Update(order);
+                _unitOfWork.Save();
+
+                // Xóa giỏ hàng
+                _unitOfWork.Cart.Remove(cart);
+                _unitOfWork.Save();
+
+                return Ok(order);
             }
             catch (Exception ex)
             {
